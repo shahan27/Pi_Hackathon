452a3435c3bfa531af9b2e6189f761f4d0f23f9d
Shahan Rahman <shahan27@live.com>
2018-11-28 21:46:49 -0500

    All files added

---
+++ pi_hackathon/TileType.java
@@ -0,0 +1,37 @@
+ 
+
+/**
+ * The {@code TileType} class represents the different
+ * types of tiles that can be displayed on the screen.
+ * @author Brendan Jones
+ *
+ */
+public enum TileType {
+
+	Fruit,
+	
+	SnakeHead,
+	
+	SnakeBody,
+	
+	Snake1,
+	
+	Snake2,
+	
+	Snake3,
+	
+	Snake4,
+	
+	Snake5,
+	
+	Snake6,
+	
+	Snake7,
+	
+	Snake8,
+	
+	Snake9,
+	
+	SnakeDot
+	
+}
